5270e5ac30b6bdb5e6fc03cb3eb2986c0503d08a
Ryan Choi <ryanchoi@ucsb.edu>
2026-01-14 11:45:58 -0800

    Create Hello World App

---
+++ lab01/ryan/hello-world/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl">Welcome to the Language Learner app</h1>
+    </div>
+  );
+}
